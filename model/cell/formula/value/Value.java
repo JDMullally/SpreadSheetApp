c21c5a6f7d3331655e986a0780262377df0d2fb3
Joseph Mullally <36319569+JDMullally@users.noreply.github.com>
2021-06-15 18:12:13 -0400

    Delete model directory

--- model/cell/formula/value/Value.java
+++
@@ -1,9 +0,0 @@
-package edu.cs3500.spreadsheets.model.cell.formula.value;
-
-import edu.cs3500.spreadsheets.model.cell.formula.Formula;
-
-/**
- * A value is a Boolean, Number, or String contents.
- */
-public interface Value extends Formula {
-}
